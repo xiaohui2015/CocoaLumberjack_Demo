d674f92ff08b2a520d93bceca1817e59caec3eae
fengxiaohui <18367107969@163.com>
2017-03-09 10:36:16 +0800

    add Podfile

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '8.0'
+target 'CocoaLumberjack_Demo' do
+use_frameworks!
+pod 'CocoaLumberjack/Swift'
+end
---
+++ Pods/Target Support Files/Pods-CocoaLumberjack_Demo/Pods-CocoaLumberjack_Demo-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_CocoaLumberjack_DemoVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CocoaLumberjack_DemoVersionString[];
+
