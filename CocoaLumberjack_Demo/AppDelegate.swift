ff6fabaf2c300c09c98905335cb4117d2501f642
fengxiaohui <18367107969@163.com>
2017-03-09 10:49:54 +0800

    simple test

--- CocoaLumberjack_Demo/AppDelegate.swift
+++ CocoaLumberjack_Demo/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+import CocoaLumberjack
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,7 +17,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        DDLog.add(DDTTYLogger.sharedInstance) // TTY = Xcode console
+        DDLog.add(DDASLLogger.sharedInstance) // ASL = Apple System Logs
+        
+        let fileLogger: DDFileLogger = DDFileLogger() // File Logger
+        fileLogger.rollingFrequency = TimeInterval(60*60*24)  // 24 hours
+        fileLogger.logFileManager.maximumNumberOfLogFiles = 7
+        DDLog.add(fileLogger)
+            
+        
+        DDLogVerbose("Verbose");
+        DDLogDebug("Debug");
+        DDLogInfo("Info");
+        DDLogWarn("Warn");
+        DDLogError("Error");
+        
+//        DDLogDebug(<#T##message: String##String#>, level: <#T##DDLogLevel#>, context: <#T##Int#>, tag: <#T##Any?#>, asynchronous: <#T##Bool#>, ddlog: <#T##DDLog#>)
+        
         return true
     }
 
